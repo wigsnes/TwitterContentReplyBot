aba84e95affd26904de53b52f0b1527279f56128
wigsnes <fredrik.wigsnes@gmail.com>
2019-10-12 18:10:17 +0200

    Rmoved _json & fixed stuff

--- twitter.py
+++ twitter.py
@@ -8,18 +8,19 @@ import jsonpickle
 # --- DEGUG --- #
 DEBUG = True
 
+
+# --- SETUP --- #
 if DEBUG:
     open('repliedTo.txt', 'w').write('')  # Clearing 'repliedTo.txt' if in debug mode
     TWEETS = jsonpickle.loads(open('mock/tweetsMock.json', 'r', encoding="utf8").read())
     USERS = jsonpickle.loads(open('mock/usersMock.json', 'r', encoding="utf8").read())
+    REPLIED_TO = set()
 else:
     USERS = set(line.strip() for line in open('data/users.txt', 'r'))
+    REPLIED_TO = set(int(line.strip()) for line in open('data/repliedTo.txt', 'r'))
 
-# --- SETUP --- #
-
-CONTENT = json.load(open('data/content.json', 'r', encoding="utf8"))
-REPLIED_TO = set(int(line.strip()) for line in open('data/repliedTo.txt', 'r'))
 repliedTo = open('data/repliedTo.txt', 'a')
+CONTENT = json.load(open('data/content.json', 'r', encoding="utf8"))
 
 
 # --- Twitter --- #
@@ -38,7 +39,7 @@ def reply(api, user, tweet, tweet_id):
 
 def getTweets(api, user, n):
     try:
-        return api.user_timeline(screen_name=user, count=n)
+        return api.user_timeline(screen_name=user, count=n, tweet_mode="extended")
     except tweepy.error.RateLimitError:
         print("Rate limit reached. Waiting 15min.")
         time.sleep(60 * 15)  # 15 min
@@ -56,7 +57,7 @@ class mock:
     def update_status(self, tweet, tweet_id):
         pass
 
-    def user_timeline(self, screen_name, count):
+    def user_timeline(self, screen_name, count, tweet_mode):
         return [Tweet(tweet, screen_name) for tweet in TWEETS[screen_name]]
 
     def get_user(self, user):
@@ -65,17 +66,42 @@ class mock:
 
 class Tweet:
     def __init__(self, tweet, user_name):
-        self.text = tweet["text"]
-        self.id = random.randint(1000000000, 9999999999)
-        self.user = User(user_name)
-        self.retweeted = tweet["retweeted"]
+        self.id = tweet["id"]
+        self.text = tweet["full_text"]
+        self.user = User(tweet["user"])
         self.created_at = tweet["created_at"]
+        self.retweet = tweet["in_reply_to_user_id"] != None
+        self.favorite_count = tweet["favorite_count"]
+        self.retweet_count = tweet["retweet_count"]
 
 
 class User:
-    def __init__(self, user_name, protected=False):
-        self.name = user_name
-        self.protected = protected
+    def __init__(self, user):
+        self.id = getValue("id",user)
+        self.name = getValue("name",user)
+        self.screen_name = getValue("screen_name",user)
+        self.followers_count = getValue("followers_count",user)
+        self.verified = getValue("verified",user)
+        self.protected = getValue("protected",user)
+        self.created_at = getValue("created_at",user)
+        self.location = getValue("location",user)
+        self.friends_count = getValue("friends_count",user)
+        self.listed_count = getValue("listed_count",user)
+        self.coordinates = getValue(["status","place", "bounding_box", "coordinates"], user)
+        self.country = getValue("country",user)
+        self.country_code = getValue("country_code",user)
+
+def getValue(val, elem):
+    if isinstance(val, list):
+        if len(val) == 1:
+            return elem[val]
+        if val[0] in elem:
+            return getValue(val[1:], elem[val[0]])
+        return None
+
+    if val in elem:
+        return elem[val]
+    return None
 
 
 # --- HELPER FUNCTIONS --- #
@@ -104,7 +130,6 @@ def main():
         for user in USERS:
             tweets = getTweets(api, user, 5)
             checkTweets(api, user, tweets)
-            
         if DEBUG:
             break
         repliedTo.flush()
@@ -116,7 +141,7 @@ def checkTweets(api, user, tweets):
     for tweet in tweets:
         if tweet.id in REPLIED_TO:
             continue
-        if tweet.retweeted:
+        if tweet.retweet:
             continue
 
         content = tweetMatchesContent(tweet.text)
